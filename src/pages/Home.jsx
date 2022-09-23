1b9fa932ec73c573da6ca586f87c94d28cd6ca6c
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-09-23 17:22:25 +0000

    chore: fragment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 function Home() {
   return (
-    <h1>Lista de Presença</h1>
+    <>
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
+    </>
   )
 }
 
